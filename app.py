d2fb5299e0d61c7d0f404e85d71729c9a4395d93
Simon Castagna <simoncastagna@Simons-MacBook-Pro.local>
2020-06-18 12:02:40 +0200

    added route decorators to app.py

--- app.py
+++ app.py
@@ -2,19 +2,47 @@ from flask import Flask, render_template
 import os
 from os import path
 if path.exists("env.py"):
-  import env 
+    import env
 SECRET_KEY = os.environ.get('SECRET_KEY')
 MONGO_DBNAME = os.environ.get('MONGO_DBNAME')
 MONGO_URI = os.environ.get('MONGO_URI')
 
 
-APP = Flask(__name__)
+app = Flask(__name__)
 
-@APP.route('/')
+
+@app.route('/')
+@app.route('/login')
 def hello_world():
-    return render_template('pages/index.html', title="The Lazy Padwan")
+    return "<h1>Login Page</h1>"
+
+
+@app.route('/register')
+def login():
+    return "<h1>Registration</h1>"
+
+
+@app.route('/home')
+def listing():
+    return "<h1>Listing of Elements</h1>"
+
+
+@app.route('/profile')
+def profile():
+    return "<h1>User Profile</h1>"
+
+
+@app.route('/entry')
+def entry():
+    return "<h1>Entry</h1>"
+
+
+@app.route('/add')
+def new_entry():
+    return "<h1>New Entry</h1>"
+
 
 if __name__ == '__main__':
-    APP.run(host=os.environ.get('HOSTNAME'),
+    app.run(host=os.environ.get('HOSTNAME'),
             port=int(os.environ.get('PORT')),
             debug=os.environ.get('DEV'))

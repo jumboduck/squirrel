7429474b3a74c7589cd3617133478fb8fc5b154f
Simon Castagna <simoncastagna@Simons-MacBook-Pro.local>
2020-06-19 12:20:11 +0200

    created registration form

--- app.py
+++ app.py
@@ -1,50 +1,57 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
 import os
 from os import path
+from forms import RegistrationForm, LoginForm
 if path.exists("env.py"):
     import env
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 MONGO_DBNAME = os.environ.get('MONGO_DBNAME')
 MONGO_URI = os.environ.get('MONGO_URI')
 
-
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 
 @app.route('/')
 @app.route('/login')
-def hello_world():
-    return render_template('pages/login.html')
-
-
-@app.route('/register')
 def login():
-    return render_template('pages/registration.html')
+    form = LoginForm()
+    return render_template('pages/login.html', title="Login", form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}.', 'success')
+        return redirect(url_for('listing'))
+    return render_template('pages/registration.html', title="Registration", form=form)
 
 
 @app.route('/home')
 def listing():
-    return render_template('pages/listing.html')
+    return render_template('pages/listing.html',  title="Listing")
 
 
 @app.route('/profile')
 def profile():
-    return render_template('pages/profile.html')
+    return render_template('pages/profile.html',  title="Profile")
 
 
 @app.route('/entry')
 def entry():
-    return render_template('pages/entry.html')
+    return render_template('pages/entry.html',  title="Entry")
 
 
 @app.route('/add')
 def new_entry():
-    return render_template('pages/new_entry.html')
+    return render_template('pages/new_entry.html',  title="New Entry")
 
 
 @app.route('/search')
 def search():
-    return render_template('pages/search.html')
+    return render_template('pages/search.html',  title="Search")
 
 
 @app.route('/logout')

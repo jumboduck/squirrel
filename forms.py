531525d18b220c276d60b33d4a4689f541e4723d
Simon Castagna <simoncastagna@Simons-MacBook-Pro.local>
2020-06-19 11:00:45 +0200

    created logic for login and registration forms

---
+++ forms.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[
+                           DataRequired(), Length(min=1, max=30)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[
+                             DataRequired(), Length(min=8)])
+    confirm_password = PasswordField('Confirm Password', validators=[
+                                     DataRequired(), EqualTo('password')])
+    submit = SubmitField('Create an Account')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[
+                           DataRequired()])
+    password = PasswordField('Password', validators=[
+                             DataRequired(), Length(min=8)])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')

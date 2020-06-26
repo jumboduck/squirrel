1309a20934e818b174debe6532be3eeed67e69cf
Simon Castagna <simoncastagna@Simons-MacBook-Pro.local>
2020-06-26 11:13:12 +0200

    created login logic and creation of session

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from os import path
 if path.exists("env.py"):
     import env
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, session
 from forms import RegistrationForm, LoginForm
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -21,19 +21,29 @@ mongo = PyMongo(app)
 bcrypt = Bcrypt(app)
 
 
+"""
+USER MANAGEMENT
+"""
+
+# Login
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    users = mongo.db.users
     if form.validate_on_submit():
-        if form.username.data == 'simon' and form.password.data == 'password':
-            flash('You are logged in', 'success')
+        login_user = users.find_one({'username' : form.username.data})
+        if login_user and bcrypt.check_password_hash(login_user['password'], form.password.data.encode('utf-8')):
+            session["username"] = form.username.data
+            flash(f'Welcome, {form.username.data}.', 'success')
             return redirect(url_for('listing'))
         else:
-            flash('Wrong username or password', 'danger')
+            flash('Wrong username or password.', 'danger')
 
     return render_template('pages/login.html', title="Login", form=form)
 
+
+# Registration
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -50,14 +60,19 @@ def register():
                 "password": hashed_password,
             })
             flash(f'Account created for {form.username.data}.', 'success')
-            return redirect(url_for('listing'))
+            return redirect(url_for('login'))
         elif existing_user:
-            flash(f'Username {form.username.data} is already in use.', 'alert')
+            flash(f'Username {form.username.data} is already in use.', 'danger')
         elif existing_email:
-            flash(f'An account already exists for {form.email.data}.', 'alert')
+            flash(f'An account already exists for {form.email.data}.', 'danger')
 
     return render_template('pages/registration.html', title="Registration", form=form)
 
+# Logout
+
+@app.route('/logout')
+def logout():
+    return "<h1>Logout</h1>"
 
 @app.route('/')
 @app.route('/listing')
@@ -84,11 +99,6 @@ def new_entry():
 @app.route('/search')
 def search():
     return render_template('pages/search.html',  title="Search")
-
-
-@app.route('/logout')
-def logout():
-    return "<h1>Logout</h1>"
 
 
 if __name__ == '__main__':

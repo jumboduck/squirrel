4d2be3578d99a114d25da60fb655a10d11ead758
Simon Castagna <simoncastagna@Simons-MacBook-Pro.local>
2020-06-28 10:37:36 +0200

    added login command to tests

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -1,32 +1,12 @@
 describe("Login page", () => {
     it("should be possible to login", () => {
-        cy.visit("/login")
-
-            .get("#email")
-            .type("test@test.com")
-
-            .get("#password")
-            .type("password")
-
-            .get("#submit")
-            .click()
-
+        cy.login("test@test.com", "password")
             .url()
             .should("match", /listing/);
     });
 
     it("should reject a wrong password", () => {
-        cy.visit("/login")
-
-            .get("#email")
-            .type("test@test.com")
-
-            .get("#password")
-            .type("wrongpassword")
-
-            .get("#submit")
-            .click();
-
+        cy.login("test@test.com", "wrongpassword");
         cy.get(".alert-danger").should("exist");
     });
 });

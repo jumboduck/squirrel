eb179d70264408121578c0216c07608e73613495
Simon Castagna <simoncastagna@Simons-MacBook-Pro.local>
2020-07-27 13:05:22 +0200

    added test to add review

---
+++ cypress/integration/addReview.js
@@ -0,0 +1,14 @@
+describe("Add review page", () => {
+    it("should be possible to add a review", () => {
+        cy.login("test@test.com", "password");
+        cy.addReview("Test Name", "Test Description", 4, true, [
+            "tag1",
+            "tag2",
+            "tag3",
+        ])
+            .url()
+            .should("match", /entry/)
+            .get("#name")
+            .should("have.value", "Test Name");
+    });
+});
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,3 +16,30 @@ Cypress.Commands.add("login", (email, password) => {
 Cypress.Commands.add("logout", () => {
     cy.visit("/").get("#logout").click();
 });
+
+Cypress.Commands.add("addReview", (name, description, rating, fav, tags) => {
+    cy.visit("/add")
+        .get("#name")
+        .type(name)
+
+        .get("#description")
+        .type(description)
+
+        .get(`label[for="rating-${rating - 1}"]`)
+        .click();
+
+    if (fav) {
+        cy.get("#is_fav").click();
+    }
+
+    if (tags) {
+        for (let i = 0; i < tags.length; i++) {
+            cy.get("#new-tag")
+                .click()
+                .get("#edit-tag-" + (i + 1))
+                .type(tags[i]);
+        }
+    }
+
+    cy.get("#submit").click();
+});

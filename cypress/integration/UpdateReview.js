13c72111255f380883f017e4515c8097e92faff0
Simon Castagna <simoncastagna@Simons-MacBook-Pro.local>
2020-07-27 19:45:57 +0200

    added tests for description update

--- cypress/integration/UpdateReview.js
+++ cypress/integration/UpdateReview.js
@@ -5,9 +5,24 @@ describe("Update entry", () => {
             .should("be.visible")
             .should("have.class", "valid-update");
     });
+
     it("should not be possible to leave the title blank", () => {
         cy.login().addReview().get("#name").clear().blur();
         cy.get("#name-feedback")
+            .should("be.visible")
+            .should("have.class", "invalid-update");
+    });
+
+    it("should be possible to update the description", () => {
+        cy.login().addReview().get("#description").type(" update").blur();
+        cy.get("#description-feedback")
+            .should("be.visible")
+            .should("have.class", "valid-update");
+    });
+
+    it("should not be possible to leave the description blank", () => {
+        cy.login().addReview().get("#description").clear().blur();
+        cy.get("#description-feedback")
             .should("be.visible")
             .should("have.class", "invalid-update");
     });

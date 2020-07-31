478b5be9b195b96ad10e77b900294b4c4829b52a
Simon Castagna <simoncastagna@Simons-MacBook-Pro.local>
2020-07-31 16:21:26 +0200

    updated view entry test to use a newly created entry

--- cypress/integration/viewEntry.js
+++ cypress/integration/viewEntry.js
@@ -1,12 +1,14 @@
 describe("View entry", () => {
-    before(() => cy.login());
+    before(() => cy.login().addReview());
     beforeEach(() => Cypress.Cookies.preserveOnce("session"));
     it("should be possible to view a review by clicking on image", () => {
+        cy.get(".logo-image").click();
         cy.get(".card:first a img").click().url().should("match", /entry/);
     });
     it("should be possible to view a review by clicking on title", () => {
         cy.get(".logo-image").click();
         cy.get(".card:first h2 a").click().url().should("match", /entry/);
+        cy.delete();
     });
     after(() => cy.logout());
 });
